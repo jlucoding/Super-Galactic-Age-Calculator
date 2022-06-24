7680db495444cb92c50b18cd0cf3c91ffb58e6c4
Jonathan Lu <jdlu.coding@gmail.com>
2022-06-24 12:09:27 -0700

    pass all tests converting Earth age to age on other planets

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -8,6 +8,18 @@ describe('EarthAge', () => {
   });
 
   test('should correctly convert Earth age to Mercury age', () => {
-    expect(reusableEarthAge.mercuryAge()).toEqual(13.2);
+    expect(reusableEarthAge.mercuryAge()).toEqual(13);
+  });
+
+  test('should correctly convert Earth age to Venus age', () => {
+    expect(reusableEarthAge.venusAge()).toEqual(34);
+  });
+
+  test('should correctly convert Earth age to Mars age', () => {
+    expect(reusableEarthAge.marsAge()).toEqual(103);
+  });
+
+  test('should correctly convert Earth age to jupiter age', () => {
+    expect(reusableEarthAge.jupiterAge()).toEqual(652);
   });
 });
--- src/age.js
+++ src/age.js
@@ -3,18 +3,18 @@ export default function EarthAge(age) {
 }
 
 EarthAge.prototype.mercuryAge = function() {
-  return this.age * 0.24
+  return Math.round(this.age * 0.24);
 };
 
 EarthAge.prototype.venusAge = function() {
-  return this.age * 0.62
+  return Math.round(this.age * 0.62);
 };
 
 EarthAge.prototype.marsAge = function() {
-  return this.age * 1.88
+  return Math.round(this.age * 1.88);
 };
 
 EarthAge.prototype.jupiterAge = function() {
-  return this.age * 11.86
+  return Math.round(this.age * 11.86);
 };
 
