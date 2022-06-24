ec41b8ebad3efd0ac724e4d3664e983b8f2d13e8
Jonathan Lu <jdlu.coding@gmail.com>
2022-06-24 14:12:51 -0700

    pass test to determine how much longer user is expected to live on Mercury

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -28,6 +28,6 @@ describe('EarthAge', () => {
   });
 
   test('should correctly calculate how much longer user is expected to live on Mercury', () => {
-    expect(reusableEarthAge.yearsLeftOnMercury()).toEqual();
+    expect(reusableEarthAge.yearsLeftOnMercury()).toEqual(4);
   });
 });
